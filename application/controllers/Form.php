e8a8d49b8f287d99feeb21abcdaa8e91c4469cd3
Archim26 <lwiniarczyk26@gmail.com>
2017-12-13 00:26:07 +0100

    takie ten

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -14,11 +14,20 @@ class Form extends CI_Controller {
 
         $this->load->library('form_validation');
 
-        $this->form_validation->set_rules('username', 'username', 'trim|required|min_length[2]|is_unique[usery.Imie]');
-        $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[8]');
-        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'trim|required|matches[password]');
-        $this->form_validation->set_rules('email', 'email', 'trim|required|valid_email|is_unique[usery.Email]');
-
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[2]|is_unique[usery.Imie]',
+    array('required'=>'Nazwa uzytkownika jest wymagana', 'is_unique'=>'Nazwa uzytkownika juz istnieje','min_length'=>'Nazwa uzytkownika musi miec conajmniej 2 znaki' ));
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[8]',
+    array('required'=>'Haslo jest wymagane','min_length'=>'Haslo musi miec conajmniej 8 znakow' ));
+        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'trim|required|matches[password]',
+    array('required'=>'Potwierdzenie hasla jest wymagane','matches'=>'Hasla musza sie zgadzac'));
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[usery.Email]',
+    array('required'=>'Adres email jest wymagany','valid_email'=>'Adres email musi byc wazny','is_unique'=>'Adres email juz istnieje'));
+        $this->form_validation->set_rules('imie', 'Imie', 'trim|required',
+    array('required'=>'Imie jest wymagane'));
+        $this->form_validation->set_rules('nazwisko', 'Nazwisko', 'trim|required',
+    array('required'=>'Nazwisko jest wymagane'));
+        $this->form_validation->set_rules('telefon', 'Telefon', 'trim|required|min_length[9]|max_length[9]',
+    array('required'=>'Telefon jest wymagany','min_length'=>'Numer telefonu musi miec 9 cyfr'));
 
         if ($this->form_validation->run() == FALSE)
         {
--- application/views/myform.php
+++ application/views/myform.php
@@ -1,25 +1,39 @@
 
 <div id="cat" class="col-md-4 col-md-offset-4">
 
+  <?php echo validation_errors(); ?>
+
     <?php echo form_open('Form'); ?>
 
-    <h5>Username</h5>
+    <label for='username'>Nazwa Uzytkownika</label></br>
     <?php echo form_error('username'); ?>
     <input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" />
 
-    <h5>Password</h5>
+    <label for='password'>Haslo</label></br>
     <?php echo form_error('password'); ?>
     <input type="text" name="password" value="<?php echo set_value('password'); ?>" size="50" />
 
-    <h5>Password Confirm</h5>
+    <label for='passconf'>Potwierdzenie Hasla</label></br>
     <?php echo form_error('passconf'); ?>
-    <input type="text" name="passconf" value="<?php echo set_value('passconf'); ?>"/>
+    <input type="text" name="passconf" value="<?php echo set_value('passconf'); ?>" size="50" />
 
-    <h5>Email Address</h5>
+    <label for='imie'>Imie</label></br>
+    <?php echo form_error('imie'); ?>
+    <input type="text" name="imie" value="<?php echo set_value('imie'); ?>" size="50" />
+
+    <label for='nazwisko'>Nazwisko</label></br>
+    <?php echo form_error('nazwisko'); ?>
+    <input type="text" name="nazwisko" value="<?php echo set_value('nazwisko'); ?>" size="50" />
+
+    <label for='telefon'>Telefon</label></br>
+    <?php echo form_error('telefon'); ?>
+    <input type="text" name="telefon" value="<?php echo set_value('telefon'); ?>" />
+
+    <label for='email'>Adres Email</label></br>
     <?php echo form_error('email'); ?>
     <div class="input-group">
         <span class="input-group-addon">@</span>
-        <input class="form-control" id="inputGroupSuccess1" aria-describedby="inputGroupSuccess1Status" type="text" name="email" value="<?php echo set_value('email'); ?>"  />
+        <input class="form-control" id="inputGroupSuccess1" aria-describedby="inputGroupSuccess1Status" type="text" name="email" value="<?php echo set_value('email'); ?>" />
     </div>
     <div><input class="btn btn-primary" type="submit" value="Submit" /></div>
 
