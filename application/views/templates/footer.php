88b5360e10f94fe59f36031a0f4c90d15e9e321d
Emis <dante.kaszka@gmail.com>
2017-12-13 20:55:10 +0100

    Refactor

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,6 +2,8 @@
     .czorny {
         background-color: floralwhite;
         position: fixed;
+        padding-top: 10px;
+        padding-bottom: 10px;
         left: 0;
         bottom: 0;
         width: 100%;
@@ -11,7 +13,7 @@
 </style>
 
 <div class="czorny">
-    <h4 id="footer">THIS PROJECT WAS CREATED BY:</br>Emil Rybaczewski & Maciej Ziolkowski & Lukasz Winiarczyk in 2017</h4>
+    <h4 id="foote">THIS PROJECT WAS CREATED BY:</br>Emil Rybaczewski & Maciej Ziolkowski & Lukasz Winiarczyk in 2017</h4>
 </div>
     </boody>
 </html>
