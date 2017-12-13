86f1f87f296b852eb869b064c1868d9f70c22ca1
Archim26 <lwiniarczyk26@gmail.com>
2017-12-13 18:54:09 +0100

    ladne jak w prawdziwym zyciu

--- application/views/login.php
+++ application/views/login.php
@@ -5,11 +5,19 @@
     <?php echo form_open('Logginc'); ?>
 
     <label for="login">Login</label>
+    <div="input-group">
+    <span class="input-group-addon"><img src="https://png.icons8.com/menedżer/win8/17/000000"></span>
     <input type="text" class="form-control" name="login"></br>
+    </div>
     <span class="text-danger"><?= form_error('login');?></span>
+
     <label for="password">Haslo</label>
+    <div="input-group">
+    <span class="input-group-addon"><img src="https://png.icons8.com/hasło/win10/17/000000"></span>
     <input type="password" class="form-control" name="password"></br>
+    </div>
     <span class="text-danger"><?= form_error('password');?></span>
+
 
     <input class="btn btn-primary" type="submit" value="Login"></br>
     <?= '<label class="text-danger">'.$this->session->flashdata("error").'</label>'; ?>
