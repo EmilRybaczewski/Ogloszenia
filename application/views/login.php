97874e7de5d3b3bcf9e4853bf011bf6a34045ae4
Archim26 <lwiniarczyk26@gmail.com>
2017-12-13 03:10:11 +0100

    lepiej ? no nie wiem

--- application/views/login.php
+++ application/views/login.php
@@ -1,23 +1,28 @@
 <style>
  .jon {
    margin-top: 10;
-   margin-left: 5;
  }
-
+.jon2 {
+  padding-right: 50;
+  padding-left: 10;
+}
 </style>
-
+<center>
 <div id="cat" class="col-md-4 col-md-offset-4">
 
     <?php echo form_open('Logginc'); ?>
 <div class="jon">
+  <div class="jon2">
     <label for="login">Login</label>
-    <input type="text" name="login">
+    <input type="text" name="login"></br>
     <span class="text-danger"><?= form_error('login');?></span>
     <label for="password">Haslo</label>
-    <input type="password" name="password">
+    <input type="password" name="password"></br>
     <span class="text-danger"><?= form_error('password');?></span>
-    <input class="btn btn-primary" type="submit" value="Login">
+  </div>
+    <input class="btn btn-primary" type="submit" value="Login"></br>
     <?= '<label class="text-danger">'.$this->session->flashdata("error").'</label>'; ?>
     </form>
 </div>
 </div>
+</center>
